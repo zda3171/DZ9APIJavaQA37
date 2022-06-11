93d4e1f52fca2fb4427b40fa1f83488deb4e83cc
D.Zubkov <zda3171@gmail.com>
2022-06-11 22:40:03 +0300

    Create FormData

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,5 @@
+public class FormDate {
+    public int day; // поле дня рождения
+    public int month; // поле меяца рождения
+    public int year; // поле года рождения
+}
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,9 +1,11 @@
 public class Post {
+    public FormDate birthday; // дата рождения
     public String name; // поле для имени
     public String passport; // поле для паспортных данных
     public String patronymic; // поле для отчества
     public String phone; // поле для номера телефона
     public String surname; // поле для фамилии
     public boolean subscription; // поле для согласия получения смс
+
 
 }
