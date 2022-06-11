c2a2c3346115733d0b714d96385e1217627f174e
D.Zubkov <zda3171@gmail.com>
2022-06-11 19:54:55 +0300

    Create Post

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспортных данных
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public String subscription; // поле для согласия получения смс
+
+}
